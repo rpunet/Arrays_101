4238af505b56ac3adf1c904919b39977dd4def61
rpunet <r.punetcorvillo@gmail.com>
2020-10-01 12:47:19 +0200

    v1.0

--- C/max_consecutive_ones.c
+++ C/max_consecutive_ones.c
@@ -1,1 +1,22 @@
-
+int findMaxConsecutiveOnes(int* nums, int numsSize)
+{
+    int i;
+    int count;
+    int max;
+    
+    i = 0;
+    count = 0;
+    max = 0;
+    while (i < numsSize)
+    {
+        if (nums[i] == 1)
+        {
+            count++;
+            max = count > max ? count : max;
+        }
+        if (nums[i] == 0)
+            count = 0;
+        i++;
+    }
+    return (max);
+}
